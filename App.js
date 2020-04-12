c2be9539a4ee035cdf4e3825d94c755c9509eda4
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-04-11 22:44:10 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function encurtadorURL() {
+  const [url, setUrl] = useState('');
+  const [name, setName] = useState('');
+  return (
+    <View >
+      <Text>Olá</Text>
+    </View>
+  );
+}
